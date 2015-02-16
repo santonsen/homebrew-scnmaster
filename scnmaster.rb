1d8d2509cb138c0439c21f8e9773c79bc345b38b
Sebastian Antonsen <sebastian@stormstudios.no>
2015-02-16 11:01:01 +0100

    added ffmpeg to dependencies

--- scnmaster.rb
+++ scnmaster.rb
@@ -3,7 +3,9 @@ require "formula"
 class Scnmaster < Formula
   homepage "http://shortcutoslo.no"
   url "https://github.com/santonsen/homebrew-scnmaster/raw/master/dist/scnmaster_0.0.2/scnmaster_0.0.2.tar.gz"
-  sha1 "b853ad3a8bb970fec16136f405eacac4591630ff"
+  sha1 "3614ff3e448f545c8a4a2e3d63be839aa9d78848"
+
+  depends_on "ffmpeg"
 
   def install
     # Remove unrecognized options if warned by configure
