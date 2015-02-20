93e280f518de5a6bfc60d5650965e55edb31e491
Sebastian Antonsen <sebastian@stormstudios.no>
2015-02-20 12:22:21 +0100

    fixed pexpect sha1 hash

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME = scnmaster
-VERSION = 0.0.3
+VERSION = 0.0.4
 MAINTAINER = Sebastian Antonsen <sebastian@stormstudios.no>
 
 BASE_DIR=dist
--- scnmaster.rb
+++ scnmaster.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class Scnmaster < Formula
   homepage "http://shortcutoslo.no"
-  url "https://github.com/santonsen/homebrew-scnmaster/raw/master/dist/scnmaster_0.0.3/scnmaster_0.0.3.tar.gz"
-  sha1 "841e7c5733c399d43b1eec8a50f5efa9129c018b"
+  url "https://github.com/santonsen/homebrew-scnmaster/raw/master/dist/scnmaster_0.0.4/scnmaster_0.0.4.tar.gz"
+  sha1 "25e1276510bd024b35ae8d285f8dd81cbd41b97e"
 
   depends_on "ffmpeg"
 
   resource "pexpect" do
     url "https://pypi.python.org/packages/source/p/pexpect/pexpect-3.3.tar.gz"
-    sha1 "841e7c5733c399d43b1eec8a50f5efa9129c018b"
+    sha1 "25e1276510bd024b35ae8d285f8dd81cbd41b97e"
   end
 
   def install
